7a3231f34c9d579265945926617ef150c8b6eba7
eselbeus <eselbeus@gmail.com>
2018-11-29 08:46:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector('div#nested div.target')
+}
+
+function increaseRankBy(n){
+  let ranks = document.querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < ranks.length; i++){
+    ranks[i].innerHTML = Number(ranks[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  return document.querySelector('div#grand-node div div div div');
+}
