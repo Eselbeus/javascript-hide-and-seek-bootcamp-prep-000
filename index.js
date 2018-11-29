ff7f0db7a5e4e809c2eb900c2cc5d870e91b6d3f
eselbeus <eselbeus@gmail.com>
2018-11-29 08:12:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector('div#nested div.target')
+}
+
+function increaseRankBy(n){
+  let ranks = document.querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < ranks.length; i++){
+    let current = ranks[i];
+    current.innerHTML.parseInt() += 1; 
+  }
+}
+
+function deepestChild(){
+  return doucment.querySelector('div#grand-node div div div div');
+}
